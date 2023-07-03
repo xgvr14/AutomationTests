fb09bb20524423c392a1fe56026540a71efeef46
xgvr14 <andrei.gavriloiu@theaccessgroup.com>
2023-07-03 16:49:28 +0300

    Initial commit

---
+++ src/test/java/Tests/LoginTests.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import config.TestBase;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginTests extends TestBase {
+    @Test
+    public void loginTest(){
+        //1.Launch app
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        //2.Enter a valid username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("tomsmith");
+        //3.Enter a valid password
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("SuperSecretPassword!");
+        //4.Click on login button
+        WebElement loginButton = driver.findElement(By.className("radius"));
+        loginButton.click();
+        //Expected: the user is successfully logged in
+        WebElement message = driver.findElement(By.id("flash"));
+        Assertions.assertEquals("You logged into a secure area!\n" +
+                "×",message.getText());
+    }
+}
---
+++ src/test/java/config/TestBase.java
@@ -0,0 +1,32 @@
+package config;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class TestBase {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeEach
+    public void createDriver() {
+     //   System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10L);
+    }
+
+    @AfterEach
+    public void destroyDriver() {
+        if (driver != null) {
+            driver.quit();
+        } else {
+            System.out.println("Driver is null!");
+        }
+
+        driver = null;
+        wait = null;
+    }
+}
